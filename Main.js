bf83a87d1d8cac8476325114686db1299e38f859
HelloWorld7894 <59885570+HelloWorld7894@users.noreply.github.com>
2021-06-23 21:52:20 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -112,7 +112,7 @@ function Detonate(){
       for(var Segment_Iter = 0; Segment_Iter < JSON_segments.length; Segment_Iter++){ //Each segment iterator
         Hidden_Operation.forEach(Element => {
           for(var In_Segment_Iter = 0; In_Segment_Iter < JSON_segments.length; In_Segment_Iter++){
-            if(JSON_segments[Segment_Iter].indexOf(JSON_segments[Segment_Iter][In_Segment_Iter]) == Element){
+            if(JSON_segments[Segment_Iter].indexOf(JSON_segments[Segment_Iter][In_Segment_Iter]) + Segment_Iter * 1036804 == Element){
               Result_Array.push(JSON_segments[Segment_Iter][In_Segment_Iter])
             }
           }
